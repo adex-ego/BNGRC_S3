79eb92c94c5af44085c125e0a753bbaf85defe5b
unfollowhailey <manantsoaranjha@gmail.com>
2026-02-16 13:31:59 +0300

    DB

---
+++ db/tables.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE db_s2_ETU003894;
+
+USE db_s2_ETU003894;
+
+CREATE TABLE besoin_type_bngrc(
+    id_besoin INT PRIMARY KEY AUTO_INCREMENT,
+    nom_besoin VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE besoin_bngrc(
+    id_besoin INT PRIMARY KEY AUTO_INCREMENT,
+    id_besoin_type INT,
+    FOREIGN KEY (id_besoin_type) REFERENCES besoin_type_bngrc(id_besoin)
+);
+
+CREATE TABLE ville_bngrc(
+    id_ville INT PRIMARY KEY AUTO_INCREMENT,
+    id_besoin_type INT,
+    nom_ville VARCHAR(255) NOT NULL
+    FOREIGN KEY (id_besoin_type) REFERENCES besoin_type_bngrc(id_besoin)
+);
